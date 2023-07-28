dd254d8d64787a535d80d3f696f17d80a611f1d7
Martin Nyaga <martin.nyaga@student.moringaschool.com>
2023-07-28 17:44:11 +0300

    renderd bots

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import BotCollection from './components/BotCollection';
+
 import './App.css';
 
 function App() {
+  //Declared variables for rendering the bots
+    const [bots, setBots] = useState([]);
+
+
+    //Fetching the bot data from deployed db.json 
+    useEffect(() => {
+      fetch('https://bot-app-data.onrender.com/bots')
+        .then((response) => response.json())
+        .then((data) => setBots(data))
+        .catch((error) => console.error('Error fetching data:', error));
+    }, []);
+
   return (
+    //Rendering bots on the web page
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BotCollection bots={bots}  />
     </div>
   );
 }
---
+++ src/components/BotCollection.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function BotCollection({bots}){
+    //Renderd bots
+        return(
+            <div>
+                {bots.map((bot) => (
+                    <div key={bot.id}>
+                        <img src={bot.avatar_url}/>
+                        <h3>Bot Name:  {bot.name}</h3>
+                        <p>Class :  {bot.bot_class}</p>
+                        <p>Catch Phrase :  {bot.catchphrase}</p>
+                        <p>Armor :  {bot.armor}</p>
+                        <p>Health :  {bot.health}</p>
+                        <p>Damage :  {bot.damage}</p>
+                        <p>Created At :  {bot.created_at}</p>
+                        <p>Updated at :  {bot.updated_at}</p>
+                        
+                    </div>
+                ))}
+            </div>
+        )
+}
